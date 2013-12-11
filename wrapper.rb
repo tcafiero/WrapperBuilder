6a291668aafb7f9954849701d9c8068b5fe037c8
Toni Cafiero <toni@cafiero.it>
2013-12-11 17:22:07 +0100

    iterazione

--- wrapper.rb
+++ wrapper.rb
@@ -2,7 +2,6 @@ require 'yaml'
 require 'erubis'
 descriptor = YAML.load_file(ARGV[0]+'.yml')
 
-details="bla bla"
 pr=File.read('wrapper_c.template')
 out=File.open(ARGV[0]+'.c','w')
 eruby=Erubis::Eruby.new(pr)
