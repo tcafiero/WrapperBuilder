152aa114c61e99b53a2080faa04d48d3888be227
Toni Cafiero <toni@cafiero.it>
2013-12-11 16:12:59 +0100

    iterazione di progetto

--- wrapper.rb
+++ wrapper.rb
@@ -10,4 +10,11 @@ out.puts eruby.result(binding())
 out.close();
 system 'astyle.exe '+ARGV[0]+'.c'
 
+pr=File.read('wrapper_h.template')
+out=File.open(ARGV[0]+'.h','w')
+eruby=Erubis::Eruby.new(pr)
+out.puts eruby.result(binding())
+out.close();
+system 'astyle.exe '+ARGV[0]+'.h'
+
 
