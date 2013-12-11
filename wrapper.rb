44fe0c4d6718cca4038fae93ac218221a20830cb
Toni Cafiero <toni@cafiero.it>
2013-12-11 11:46:55 +0100

    iterazione di progetto

--- wrapper.rb
+++ wrapper.rb
@@ -1,13 +1,13 @@
 require 'yaml'
 require 'erubis'
-descriptor = YAML.load_file('descriptor1.yml')
+descriptor = YAML.load_file(ARGV[0]+'.yml')
 
 details="bla bla"
-pr=File.read('pr.t')
-out=File.open('wrapper.c','w')
+pr=File.read('wrapper_c.template')
+out=File.open(ARGV[0]+'.c','w')
 eruby=Erubis::Eruby.new(pr)
 out.puts eruby.result(binding())
 out.close();
-system 'astyle.exe wrapper.c'
+system 'astyle.exe '+ARGV[0]+'.c'
 
 
