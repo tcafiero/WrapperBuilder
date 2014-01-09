ff435426ab4d8965a23ab4605d7447528207f166
Toni Cafiero <toni@cafiero.it>
2014-01-09 14:37:29 +0100

    Genera codice ma non genera .xml per VF027 (?)

--- wrapper.rb
+++ wrapper.rb
@@ -3,7 +3,6 @@ require 'erubis'
 require 'xmlsimple'
 descriptor = YAML.load_file(ARGV[0]+'.yml')
 xmlfile=File.open(ARGV[0]+'.xml','w')
-xmlfile.puts XmlSimple.xml_out(descriptor)
 
 pr=File.read('wrapper_c.template')
 out=File.open(ARGV[0]+'.c','w')
@@ -54,5 +53,7 @@ pr=File.read('SWCC_Output_csv.template')
 out=File.open(ARGV[0]+'_SVCC_Output.csv','w')
 eruby=Erubis::Eruby.new(pr)
 out.puts eruby.result(binding())
+xmlfile.puts XmlSimple.xml_out(descriptor)
+
 out.close();
 
